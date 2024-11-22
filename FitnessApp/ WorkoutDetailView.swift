a8ebe96d24f99654095dae680ef96ab28a6ab461
Sandra7560 <143098951+Sandra7560@users.noreply.github.com>
2024-11-22 10:59:29 -0500

    initialcommit

---
+++ FitnessApp/ WorkoutDetailView.swift
@@ -0,0 +1,8 @@
+//
+//   WorkoutDetailView.swift
+//  FitnessApp
+//
+//  Created by sandra sudheendran on 2024-11-22.
+//
+
+import Foundation
---
+++ FitnessApp/HomePage.swift
@@ -0,0 +1,8 @@
+//
+//  HomePage.swift
+//  FitnessApp
+//
+//  Created by sandra sudheendran on 2024-11-22.
+//
+
+import Foundation
